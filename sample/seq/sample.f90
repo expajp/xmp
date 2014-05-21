65d6bd8b6a19b7da2d9101f4ac77be9a8afe3e63
expajp <expajp@gmail.com>
2014-05-21 22:26:09 +0900

    rewrite sample/seq/sample.f90

--- sample/seq/sample.f90
+++ sample/seq/sample.f90
@@ -9,65 +9,56 @@ program sample
   use sample_size
   implicit none
 
-! Set Variables and Arrays
+! Set Variables
   integer :: i, j, k
-
-  real :: x(mimax,mjmax,mkmax)
-  real :: y(mimax,mjmax,mkmax)
 
 ! Variables for XMP
   integer :: cpu0, cpu1, cpu2, t_rate, t_max, diff
 
-! initialize array
-  call system_clock(cpu0)
+! Set Arrays
+  real :: x(mimax,mjmax,0:mkmax+1)
+  real :: y(mimax,mjmax,0:mkmax+1)
 
+  x = y = 0.0
+
+! --- count start --- !
+
+! initialize array
   do k=1, mkmax
      do j=1, mjmax
         do i=1, mimax
            x(i, j, k) = i + j + k
-           y(i, j, k) = 0
         end do
      end do
   end do
 
-    call system_clock(cpu1)
+! message transfer
+
+! --- count split 01 --- !
 
 ! main loop
-
   do k=1, mkmax
-     do j=2, mjmax-1
-        do i=2, mimax-1
+     do j=1, mjmax
+        do i=1, mimax
+
            ! substitute to y
-           if(k == 1) then
-              y(i, j, k) = x(i+1, j+1, k+1)
-           else if(k == mkmax) then
-              y(i, j, k) = x(i-1, j-1, k-1)
-           else
-              y(i, j, k) = x(i-1, j-1, k-1) + x(i+1, j+1, k+1)
-           end if
+           y(i, j, k) = x(i, j, k-1) + x(i, j, k+1)
 
            ! output for debug
-           !if(i == 2 .and. j == 2 .and. mod(k,mkmax/nprocs) == 1) then
-           !   write(*,'(A,3(i3,A),f5.1)') 'y(', i, ',', j, ',', k, ') = ',y(i, j, k)
-           !end if
+           ! write(*,'(A,3(i3,A),f5.1)') 'y(', i, ',', j, ',', k, ') = ',y(i, j, k)
 
         end do
      end do
   end do
-  
-  call system_clock(cpu2, t_rate, t_max)
-  if ( cpu2 < cpu0 ) then
-    diff = t_max - cpu0 + cpu2
-  else
-    diff = cpu2 - cpu0
-  endif
 
-!     write(*,*)
-!     write(*,*) 'The time was counted.'
-!     write(*,'(A,f8.5)') 'Initialize (s): ',cpu1-cpu0
-!     write(*,'(A,f8.5)') 'Caliculate (s): ',cpu2-cpu1
-!     write(*,'(A,f8.5)') 'Total (s): ',cpu2-cpu0
-!     write(*,*)
-     write(*,'(f9.6)') (cpu2-cpu0)/dble(t_rate)
+! --- count stop --- !
+
+! output
+  write(*,*)
+  write(*,*) 'The time was counted.'
+  write(*,'(A,f8.5)') 'Initialize (s): ',cpu1-cpu0
+  write(*,'(A,f8.5)') 'Caliculate (s): ',cpu2-cpu1
+  write(*,'(A,f8.5)') 'Total (s): ',cpu2-cpu0
+  write(*,*)
 
 end program sample
