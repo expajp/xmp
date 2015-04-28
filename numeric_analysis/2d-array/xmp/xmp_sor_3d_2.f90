efb25f114d2d07a5c8d1957f45b56bae2fc0c684
expajp <expajp@gmail.com>
2015-04-28 21:30:18 +0900

    finished retaking xmp_sor_3d_2

--- numeric_analysis/2d-array/xmp/xmp_sor_3d_2.f90
+++ numeric_analysis/2d-array/xmp/xmp_sor_3d_2.f90
@@ -23,9 +23,9 @@ program xmp_sor_3d_2
   real(8) :: region_x_length, region_y_length, region_z_length
   real(8) :: h_x, h_y, h_z
 
-  real(8) :: a_diag
   real(8) :: x(-l+2:sf+l-1, n-1), x_old(sf, n-1), x_diff(sf, n-1) ! object of calc
-  real(8) :: a_h_x(0:sf+1, n-1), a_h_y(-l+2:sf, n-1), a_h_z(sf, n-1) ! left-hand side
+  real(8) :: a_h_x_upper(sf, n-1), a_h_y_upper(sf, n-1), a_h_z_upper(sf, n-1) ! left-hand side
+  real(8) :: a_h_x_lower(sf, n-1), a_h_y_lower(sf, n-1), a_h_z_lower(sf, n-1), a_diag ! left-hand side
   real(8) :: b(sf, n-1) ! right_hand side
   integer :: coef_h_x, coef_h_y
 
@@ -46,14 +46,13 @@ program xmp_sor_3d_2
   !$xmp nodes p(4)
   !$xmp template t(n-1)
   !$xmp distribute t(block) onto p
-  !$xmp align(*,k) with t(k) :: x, x_old, x_diff, a_h_x, a_h_y, a_h_z, b
+  !$xmp align(*,k) with t(k) :: x, x_old, x_diff, b
+  !$xmp align(*,k) with t(k) :: a_h_x_upper, a_h_y_upper, a_h_z_upper
+  !$xmp align(*,k) with t(k) :: a_h_x_lower, a_h_y_lower, a_h_z_lower
   !$xmp shadow x(0,1)
-  !$xmp shadow a_h_z(0,1)
 
   !myrank = xmp_node_num()
   !nprocs = xmp_num_nodes()
-
-  !write(*,'(A,i2,A)') "Hello! My Rank is ", myrank, "."
 
   ! constants
   region_x_length = region_x_upper - region_x_lower ! = 1
@@ -65,23 +64,39 @@ program xmp_sor_3d_2
   
   ! matrix
   a_diag = -2.0d0*((1.0d0/h_x**2)+(1.0d0/h_y**2)+(1.0d0/h_z**2))
-  a_h_x = 0.0d0
-  a_h_y = 0.0d0
-  a_h_z = 0.0d0
+
+  ! array指示文
+  a_h_x_upper = 0.0d0
+  a_h_y_upper = 0.0d0
+  a_h_z_upper = 0.0d0
+  a_h_x_lower = 0.0d0
+  a_h_y_lower = 0.0d0
+  a_h_z_lower = 0.0d0
 
   !$xmp loop on t(j)
   do j = 1, n-1
      do i = 1, sf
-        if(mod(i,l-1) /= 0) a_h_x(i, j) = 1.0d0/h_x**2
-        if(i <= (l-1)*(m-2)) a_h_y(i, j) = 1.0d0/h_y**2
-        if(j /= 0 .and. j /= n-1) a_h_z(i, j) = 1.0d0/h_z**2
+
+        if(mod(i,l-1) /= 0) a_h_x_upper(i, j) = 1.0d0/h_x**2
+        if(i <= (l-1)*(m-2)) a_h_y_upper(i, j) = 1.0d0/h_y**2
+        if(j /= n-1) a_h_z_upper(i, j) = 1.0d0/h_z**2
+
+        if(mod(i,l-1) /= 1) a_h_x_lower(i, j) = 1.0d0/h_x**2
+        if(i > l-1) a_h_y_lower(i, j) = 1.0d0/h_y**2
+        if(j /= 1) a_h_z_lower(i, j) = 1.0d0/h_z**2
+
      end do
   end do
 
-  !$xmp reflect(a_h_z)
-
   ! right-hand side
-  b = 0.0d0
+  ! array指示文
+  !$xmp loop on t(j)
+  do j = 1, n-1
+     do i = 1, sf
+        b(i, j) = 0.0d0
+     end do
+  end do
+
   coef_h_x = 0
   coef_h_y = 0
 
@@ -98,9 +113,14 @@ program xmp_sor_3d_2
   norm_diff = 0.0d0
   norm_x = 0.0d0
 
-  ! 初期化サボって大丈夫かな？
-  x = 0.0d0
-  x_old = 0.0d0
+  ! array指示文
+  !$xmp loop on t(j)
+  do j = 1, n-1
+     do i = -l+2, sf+l-1
+        x(i, j) = 0.0d0
+        if(i > 0 .and. i <= sf) x_old(i, j) = 0.0d0
+     end do
+  end do
 
   !$xmp task on p(1)
   write(*,*) "epsilon = ", epsilon
@@ -111,6 +131,7 @@ program xmp_sor_3d_2
 
   do
      ! x_old = x
+     ! arrayというかgmove?
      !$xmp loop on t(j)
      do j = 1, n-1
         do i = 1, sf
@@ -119,25 +140,21 @@ program xmp_sor_3d_2
      end do
 
      ! calculate new vector
-     ! 未定義領域の参照があるため、エラーが出たら一応ここを疑ってみること
      !$xmp loop on t(j)
      do j = 1, n-1
         do i = 1, sf
+
            x(i, j) = (b(i, j) &
-                - a_h_x(i-1, j)*x(i-1, j) - a_h_x(i, j)*x(i+1, j) &
-                - a_h_y(i-l+1, j)*x(i-l+1, j) - a_h_y(i, j)*x(i+l-1, j) &
-                - a_h_z(i, j-1)*x(i, j-1) - a_h_z(i, j)*x(i, j+1) ) &
+                - a_h_x_lower(i, j)*x(i-1, j) - a_h_x_upper(i, j)*x(i+1, j) &
+                - a_h_y_lower(i, j)*x(i-l+1, j) - a_h_y_upper(i, j)*x(i+l-1, j) &
+                - a_h_z_lower(i, j)*x(i, j-1) - a_h_z_upper(i, j)*x(i, j+1) ) &
                 * (omega/a_diag) + (1-omega)*x(i, j)
+           
         end do
      end do
      
      ! message transfer
      !$xmp reflect(x)
-
-     !!$xmp loop on t(j)
-     !do j = 1, n-1
-     !   write(*, '(A, i3, A, i3, A, f10.5)') "count = ", count, " x(1, ", j, ") = ", x(1, j)
-     !end do
 
      ! calculate norm
      ! x_diff = x - x_old
@@ -161,13 +178,14 @@ program xmp_sor_3d_2
 
      ! check convergence
      if(norm_diff <= epsilon*norm_x) exit
-     
+
      ! preparation of next iter
      count = count+1
 
      ! shinchoku dou desuka?
      !$xmp task on p(1)
      if(mod(count,500) == 0) then
+        ! write(*, *) "iteration: ", count, "relative error = ", norm_diff/norm_x
         write(*, *) "iteration: ", count
         write(*, *) "relative error = ", norm_diff/norm_x
      end if
@@ -181,17 +199,15 @@ program xmp_sor_3d_2
 
   end do
 
-  !$xmp barrier
-
   ! output
   !$xmp task on p(1)
   write(*, *) "iteration: ", count
   !$xmp end task
 
   ! output
-  !$xmp task on p(2)
+  !$xmp task on p(1)
   do i = 1, l-1
-     write(*, '(i3, e15.5)') i, x((l-1)*((m-1)/2+1)+i, (n-1)/2)
+     write(*, '(i3, e15.5)') i, x((l-1)*((m-1)/2)+i, 1)
   end do
   !$xmp end task
 
@@ -200,4 +216,5 @@ program xmp_sor_3d_2
 end program xmp_sor_3d_2
 
 ! 2015/04/01
+! 2015/04/28
 ! written by Shu OGAWARA
