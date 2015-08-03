534ed44cd20c26fc4f2890b67fb1be4b98900b33
expajp <expajp@gmail.com>
2015-08-03 23:39:50 +0900

    update 3-2

--- math_modeling/2_report/3-2.sh
+++ math_modeling/2_report/3-2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-for i in 0 25 50 100
+for i in 0 25 50
 do
-t=`echo "scale=2; i/100" | bc`
+t=`echo "scale=2; ${i}/100" | bc`
 echo ${t}
 
 gnuplot<<EOF
@@ -10,7 +10,7 @@ set xl "x";set yl "y";set zl "u"
 
 set terminal postscript enhanced color
 set output "3-2-${i}.eps"
-splot "3-2-${i}.out" using 2:3:4 title "t=${t}" with line
+splot "3-2-${i}.out" using 2:3:4 title "t=0.${t}" with line
 
 set terminal png
 set output "3-2-${i}.png"
@@ -19,5 +19,20 @@ replot
 exit
 EOF
 done
+
+gnuplot<<EOF
+set xl "x";set yl "y";set zl "u"
+
+set terminal postscript enhanced color
+set output "3-2-100.eps"
+splot "3-2-100.out" using 2:3:4 title "t=1.00" with line
+
+set terminal png
+set output "3-2-100.png"
+replot
+
+exit
+EOF
+
 
 echo "done."
