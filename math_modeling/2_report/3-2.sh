2070fcb290b312b272e548a32daef4bca67162c4
expajp <expajp@gmail.com>
2015-08-03 23:59:24 +0900

    update twice

--- math_modeling/2_report/3-2.sh
+++ math_modeling/2_report/3-2.sh
@@ -10,7 +10,7 @@ set xl "x";set yl "y";set zl "u"
 
 set terminal postscript enhanced color
 set output "3-2-${i}.eps"
-splot "3-2-${i}.out" using 2:3:4 title "t=0.${t}" with line
+splot "3-2-${i}.out" using 2:3:4 title "t=${t}" with line
 
 set terminal png
 set output "3-2-${i}.png"
