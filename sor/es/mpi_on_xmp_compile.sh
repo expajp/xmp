aad31b75b4da17fec6d38b6ff6cf5de3c4422905
expajp <expajp@gmail.com>
2015-12-19 07:08:35 +0900

    add option for optimization

--- sor/es/mpi_on_xmp_compile.sh
+++ sor/es/mpi_on_xmp_compile.sh
@@ -2,5 +2,5 @@
 
 #$1で指定されたファイルをmpisxでコンパイルして、data領域に移す
 rm -f /S/data00/G5055/y0405/a.out 2>/dev/null
-sxmpif90 -C hopt $1
+xmpf90 -C hopt -I /SX/usr/include/f03/ $1
 cp a.out /S/data00/G5055/y0405/
